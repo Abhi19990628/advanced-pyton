60b88aa39863ebed8fbda0447e6a613dd6014874
abhishek kumar <abhiv5976@gmail.com>
2024-07-11 02:45:43 +0530

    user difined exception handling

--- problem/Exception Handling.py
+++ problem/Exception Handling.py
@@ -1,38 +1,57 @@
-import sys
+# import sys
 
-no_1 = int(input("Enter your frist no : "))
-no_2 = int(input("enter your secound no :"))
+# no_1 = int(input("Enter your frist no : "))
+# no_2 = int(input("enter your secound no :"))
+# try:
+#     div = no_1//no_2
+#     print(div)
+# except :
+#     print(sys.exc_info()[0]) # in exception hadling get information for class
+#     print(sys.exc_info()[1]) # in Exception handling get infomation 
+    
+    
+    
+# # in Execption handling using raise for print error for user 
+# try:
+#     age = int(input("Enter your age :"))
+#     if age < 0:
+#         raise ValueError()
+#     print("Your age is :", age)
+# except ValueError:
+#     print("plz fillout the real value")
+    
+# print("Everything is good")
+
+# # first way to solve
+
+
+# try:
+#     age = int(input("Enter your age :"))
+#     if age < 0:
+#         raise ValueError("plz fill the real value")
+#     print("Your age is :", age)
+    
+# except ValueError as obj:
+    
+#     print(obj)
+    
+# print("Everything is good")
+
+
+#user defined Exception handling\
+
+class five_division_error(Exception):
+    pass
+    
 try:
-    div = no_1//no_2
-    print(div)
-except :
-    print(sys.exc_info()[0]) # in exception hadling get information for class
-    print(sys.exc_info()[1]) # in Exception handling get infomation 
-    
-    
-    
-# in Execption handling using raise for print error for user 
-try:
-    age = int(input("Enter your age :"))
-    if age < 0:
-        raise ValueError()
-    print("Your age is :", age)
-except ValueError:
-    print("plz fillout the real value")
-    
-print("Everything is good")
+    v1=int(input("Enter your first no :"))
+    v2=int(input("Enter your secound no is :"))
+    if v2 == 5:
+        raise five_division_error("cannot be dividec by 5")
+    a=v1//v2
+    print(a)
 
-# first way to solve
-
-
-try:
-    age = int(input("Enter your age :"))
-    if age < 0:
-        raise ValueError("plz fill the real value")
-    print("Your age is :", age)
+except (five_division_error,ZeroDivisionError) as obj:
+    print (obj)
     
-except ValueError as obj:
-    
-    print(obj)
-    
-print("Everything is good")
+print("rest of code")
