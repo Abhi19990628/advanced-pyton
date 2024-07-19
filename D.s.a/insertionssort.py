6da31ae3d640653dac258c5db751900d279d97fb
abhishek kumar <abhiv5976@gmail.com>
2024-07-19 15:29:36 +0530

    daily work

---
+++ D.s.a/insertionssort.py
@@ -0,0 +1,17 @@
+def insertionsort(list1):
+    for i in range(1, len(list1)):
+        temp=list1[i]
+    
+        j=i-1
+        while j>=0 and temp<list1[j]:
+           list1[j+1]=list1[j]
+           j-=1
+        list1[j+1]=temp
+           
+mylist=[11,21,22,123,32,34]
+insertionsort(mylist)
+print(mylist)
+
+
+        
+    
---
+++ problem/01.dict.py
@@ -0,0 +1,11 @@
+abhishek={
+    "abhishek" : 100,
+    "ravi" : 10 ,
+    "rishab" : 120,
+}
+
+print(abhishek.items())
+print(abhishek.keys())
+print(abhishek.values())
+abhishek.update({"abhishek": 20})
+print(abhishek.values())
