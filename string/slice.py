2cbacad8759aec31875bd02e1499ee5f58c94ad2
abhishek kumar <abhiv5976@gmail.com>
2024-08-16 23:27:49 -1200

    Update slice.py

--- string/slice.py
+++ string/slice.py
@@ -1,3 +1,8 @@
 number=[1,2,13,11,233,23]
 serial=sorted(number)
 print(serial[1:-1])
+
+
+abhishek=[1,2,3,4,5,6]
+# i want print the number 2 to 5  like 2,3,4,5 and using slicing 
+print(abhishek[1:-1])
