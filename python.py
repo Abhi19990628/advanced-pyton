2439cb6fb13e294cbe9c7209662934e5df9582c8
abhishek kumar <abhiv5976@gmail.com>
2024-08-26 23:53:46 +0530

    Update python.py

--- python.py
+++ python.py
@@ -803,3 +803,18 @@ printValue("3","4") #7
 
 #----------------------------------------#
 2.10
+Question:
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+Hints:
+
+Use + to concatenate the strings
+
+Solution
+def printValue(s1,s2):
+	print s1+s2
+
+printValue("3","4") #34
+
+#----------------------------------------#
+2.10
