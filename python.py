17234a5758d98349b55563ca880f4445c04971b8
abhishek kumar <abhiv5976@gmail.com>
2024-08-27 00:02:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1231,3 +1231,26 @@ print squaredNumbers
 
 
 #----------------------------------------#
+7.2
+
+Question:
+Define a class named American which has a static method called printNationality.
+
+Hints:
+
+Use @staticmethod decorator to define class static method.
+
+Solution
+class American(object):
+    @staticmethod
+    def printNationality():
+        print "America"
+
+anAmerican = American()
+anAmerican.printNationality()
+American.printNationality()
+
+
+
+
+#----------------------------------------#	      
