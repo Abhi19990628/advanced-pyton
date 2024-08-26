4cacb92607d161300101a88081fb5ddf56ba4f3c
abhishek kumar <abhiv5976@gmail.com>
2024-08-27 00:14:58 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2036,3 +2036,19 @@ print t
 print zlib.decompress(t)
 
 #----------------------------------------#
+Question:
+
+Please write a program to print the running time of execution of "1+1" for 100 times.
+
+
+
+Hints:
+Use timeit() function to measure the running time.
+
+Solution:
+
+from timeit import Timer
+t = Timer("for i in range(100):1+1")
+print t.timeit()
+
+#----------------------------------------#
