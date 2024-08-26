6151b011622d1604e9d041b78c45ae70954f5275
abhishek kumar <abhiv5976@gmail.com>
2024-08-27 00:06:40 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1511,3 +1511,18 @@ print re.findall("\d+",s)
 
 
 #----------------------------------------#
+Question:
+
+
+Print a unicode string "hello world".
+
+Hints:
+
+Use u'strings' format to define unicode string.
+
+Solution:
+
+unicodeString = u"hello world!"
+print unicodeString
+
+#----------------------------------------#
