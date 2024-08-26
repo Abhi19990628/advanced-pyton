27118912b4aee37a0cc34896288a69a11953869e
abhishek kumar <abhiv5976@gmail.com>
2024-08-27 00:15:38 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2069,3 +2069,22 @@ shuffle(li)
 print li
 
 #----------------------------------------#
+Question:
+
+Please write a program to shuffle and print the list [3,6,7,8].
+
+
+
+Hints:
+Use shuffle() function to shuffle a list.
+
+Solution:
+
+from random import shuffle
+li = [3,6,7,8]
+shuffle(li)
+print li
+
+
+
+#----------------------------------------#
